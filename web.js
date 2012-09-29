f58bdeafb5929cacac864a9da515a62d61ecf283
Mike Milner <mike.milner@canonical.com>
2012-09-29 14:13:13 -0300

    Add basic express structure and move files to root.

--- web.js
+++ web.js
@@ -1,12 +1,36 @@
 var express = require('express');
 
-var app = express.createServer(express.logger());
+
+// Configure app
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.configure('development', function() {
+  app.locals.pretty = true;
+});
+
+// Set up middleware
+app.use(express.responseTime());
+app.use(express.logger());
+app.use(express.favicon());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({"secret": "BadWolf"}));
+//app.use(flash());
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
+
+// Start listening
+var port = process.env.PORT || 5000;
+var server = app.listen(port, function(){
+    console.log("Server listening on " + port);
+});
+
+
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('index')
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
