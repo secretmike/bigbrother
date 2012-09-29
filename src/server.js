4148f7d0b1d87ada6a2bd97e52c96894e8921e2e
TC <111062c@acadiau.ca>
2012-09-29 13:55:07 -0300

    Added / testing route

--- src/server.js
+++ src/server.js
@@ -8,3 +8,9 @@ app.set('view engine', 'jade');
 
 var server = app.listen(8000);
 console.log('Server listening at http://127.0.0.1:8000/');
+
+// Routes
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
