cb03bdfb952ed95435c7ab37da50a2fb63ac0a0b
TC <111062c@acadiau.ca>
2012-09-29 13:53:41 -0300

    Created server.js

---
+++ src/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+
+
+var server = app.listen(8000);
+console.log('Server listening at http://127.0.0.1:8000/');
